14eec71f2efee956b70fe59549227c2a40b662d8
Tiloy84 <tiloy67@gmail.com>
2020-07-15 17:17:41 +0000

    lines 4 and 7

---
+++ main.py
@@ -0,0 +1,12 @@
+example = "example"
+print("this is an",example,"and has no errors.")
+
+name = "ellia de"
+name = "Earl sweatshirt"
+vacation  = "Nosara"
+name ="painting and skateboarding"
+today="06/20/22"
+
+print("My name is nameee,and my favorite artist is,"
+print("I love to go on vacation to vacation,and my two main hobbies are hobby")
+print("Today's date is:",today)
